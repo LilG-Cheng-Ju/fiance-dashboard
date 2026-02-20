6b9d47c9793725fe48592521ab9fcc5e7be839bb
Clement Lee <billlcj104@gmail.com>
2026-02-21 01:17:51 +0800

    release 0.2.2-beta

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  appVersion: '0.2.1-beta',
+  appVersion: '0.2.2-beta',
   firebaseConfig: {
     apiKey: "AIzaSyBHKMBh7dOVQOM20TmD0qaMB1tjLKW2Cfk",
     authDomain: "finance-dashboard-487816.firebaseapp.com",
